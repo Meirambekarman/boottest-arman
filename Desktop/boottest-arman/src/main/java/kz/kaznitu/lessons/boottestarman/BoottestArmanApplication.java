cb42b1346df7f10565e4a00465257dbc3347ad5e
Arman <meirambek.arman.97@gmail.com>
2018-03-06 10:27:14 +0600

    #1 Проектыны құрдым. Енді жұмысқа кірісемін.

---
+++ Desktop/boottest-arman/src/main/java/kz/kaznitu/lessons/boottestarman/BoottestArmanApplication.java
@@ -0,0 +1,12 @@
+package kz.kaznitu.lessons.boottestarman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoottestArmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoottestArmanApplication.class, args);
+	}
+}
